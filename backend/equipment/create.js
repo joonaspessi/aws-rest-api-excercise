3ed82ceade05cf399e2b009fd466c97bdd4ed1a2
Joonas Pessi <pessi@Joonass-MacBook-Pro.local>
2019-11-03 16:44:52 +0200

    Duplicate check to create api
    * Use conditionExpression for checking duplicate values

--- backend/equipment/create.js
+++ backend/equipment/create.js
@@ -9,7 +9,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.create = async event => {
   let equipment;
   try {
-    equipment = JSON.parse(event.body);
+    equipment = createEquipment(JSON.parse(event.body));
   } catch (e) {
     return {
       statusCode: 400,
@@ -24,28 +24,31 @@ module.exports.create = async event => {
 
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
-    Item: createEquipment(equipment)
+    Item: equipment,
+    ConditionExpression: 'id <> :idVal',
+    ExpressionAttributeValues: {
+      ':idVal': equipment.id
+    }
   };
 
   try {
     await dynamoDb.put(params).promise();
-  } catch (e) {
     return {
-      statusCode: error.statusCode || 501,
+      statusCode: 200,
       headers: {
-        'Content-Type': 'text/plain',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
-      body: "Couldn't create the equipment item."
+      body: JSON.stringify(params.Item)
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 501,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(error)
     };
   }
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify(params.Item)
-  };
 };
